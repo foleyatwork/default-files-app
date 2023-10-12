48c04e19b013b61d98df2b3de2925a89ea754bf0
Kevin Foley <kevin.james.foley@gmail.com>
2023-10-12 14:01:04 +0100

    cleanup

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -119,20 +119,16 @@ export async function POST(request: Request) {
     method: 'POST',
   });
 
-  console.log('====================')
   const createFileData = await createFileRes.json();
   if (!('uploadUrl' in createFileData)) {
     throw new Error('Could not retrieve file upload URL');
   }
 
-  // https://ridebill-userfiles-mobilehub-310871815.s3.us-west-2.amazonaws.com/protected/us-west-2%3Aab8c27c0-4948-4de0-a870-f6dbd12d9b92/files/120a95ed-5181-47eb-b21b-ce94f3021c19/2023-10-12T10%3A34%3A07Z/eyee.gif?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIAQJQGUNGITCVB5527%2F20231012%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20231012T103407Z&X-Amz-Expires=900&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEPn%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMiJHMEUCIQCbCUSCb1pY%2BfK2BeNq0nRVDsxwMiZw7rQKpQlUaX3mNwIgOojP2XAd1sLChIqLTBcV9urvf0wPkje2OPa5mtAyDoQqjQMIEhAGGgwwMjA0MTUwMTUzMTMiDHw1UC6GHGfcs%2F3ZTCrqAqP%2FiQv2xG2MW8EmOjW701%2F67WNZgQHHgzK8Hv5m0ktvviM3Df7QAx6x6Q4J8BJHeeM4SbnFsbB3x1G2AOZMmZUlYPNC7cG3gPdeEKS7UGr6DfOclvcy9TZ2EThFlaxOPQ5M706tuqpZaKKRzGCvWmDFFSgIEtYdu3U6vLdLCh6rQ8O%2BBQKLacqwdUUPy63XNNjpjh%2BuM%2FmW2yOLhCTT4j8cHduKmrybWEudTJLqusJjmBAwM6D5kCOa7QdXqDFDdxOhTO0raYt6I0UxCkP1ZLziNKo8zwhHWFMWdHnyFhU%2FLCwvfWruUiIMl7PvG9GzHR1NMrHbYM4D7FdB5GD9eaD8AkxpgCFVxD2KXXZmoJfo7SJ%2FnF%2FPqDERCKkXo4AqmjiywTQnkzHkYU1kE7M0pOkwn6VST7ooabcDg%2BsR6J3MmP%2FTfbUW31S6PUiSlRinNKdHRHFjSYRj8LanDhFgcbcdBZBbM2m81x7TMIvjnqkGOp0Bm1PgklIYRqerxzIfHYMkBhVJKJfFb9q5xgU20EDcakslj7%2FrVoQqbpajKAYFe1VtyhAIsR7bGA3dmH1E8rnNF1F5oo6voAJak8gKLWp0GLS15JgpfKKmnA6zQ74TB4pFkmqXu3RxboIzp5MWkBX67tcLBsjc3dtcvudbrv8U1ViahOOKiKv46To4mFkNmu401bZKN05BebKkydBWvg%3D%3D&X-Amz-SignedHeaders=host&X-Amz-Signature=643afc65cea6cf64837f5709da2a1e2582206393fe5891b2e07487bcac5b9ca2
-  console.log('====================')
   const file = await GetFromS3Bucket('StrengthProgram.pdf');
-  console.log({file})
   const uploadFileToS3Res = await fetch(createFileData.uploadUrl, {
     body: file as BodyInit,
     // @ts-ignore
-    duplex: 'half',
+    duplex: true,
     method: 'PUT',
     headers: {
       ContentType: 'application/octet-stream',
