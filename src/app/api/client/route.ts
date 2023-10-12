b06b69434287433f1342840994707b87beb90e2b
Kevin Foley <kevin.james.foley@gmail.com>
2023-10-12 13:48:54 +0100

    logging

--- src/app/api/client/route.ts
+++ src/app/api/client/route.ts
@@ -75,9 +75,7 @@ const isCreateFileRes = (data: any): data is CreateFileRes => {
 }
 
 export async function POST(request: Request) {
-  console.log({ request });
   const data = await request.json();
-  console.log({ data });
   if (!isClientCreatedWebhook(data)) {
     throw new Error('Invalid request');
   }
@@ -121,6 +119,7 @@ export async function POST(request: Request) {
     method: 'POST',
   });
 
+  console.log('====================')
   const createFileData = await createFileRes.json();
   if (!('uploadUrl' in createFileData)) {
     throw new Error('Could not retrieve file upload URL');
